8d742582b0f139da729b48a6a8e594b4840fc3f0
fedechaves <federicochavesf@gmail.com>
2022-12-06 12:03:30 -0300

    feat: added errors, stories views, setted stories in index

--- routes/index.js
+++ routes/index.js
@@ -14,12 +14,15 @@ router.get('/', ensureGuest, (req, res) => {
 router.get('/dashboard', ensureAuth, async (req, res) => {
     try {
         const stories = await Story.find({ user: req.user.id }).lean()
+        res.render('dashboard',{
+            name: req.user.firstName,
+            stories
+        })
     } catch (err) {
         console.error(err)
+        res.render('error/500')
     }
-    res.render('dashboard',{
-    name: req.user.firstName,
-})
+    
 })
 
 module.exports= router
